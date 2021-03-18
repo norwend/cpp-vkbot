18d2087947c0cb8254aea9301b4c714cb057a4ab
Stepan Ilyin <norwend@tuta.io>
2021-03-18 22:01:23 +0300

    .

---
+++ include/api.hpp
@@ -0,0 +1,12 @@
+#ifndef API
+#define API
+
+namespace api {
+	inline const char* vkurl = "https://api.vk.com/method/";
+// поменять!
+	inline const char* token = "";
+	inline const int id = 0;
+}
+
+
+#endif
