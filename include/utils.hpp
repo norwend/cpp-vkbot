b3e765121a2d96eea682c82f889bab27d0ecccf5
Stepan Ilyin <norwend@tuta.io>
2021-03-29 01:01:16 +0300

    header change

--- include/utils.hpp
+++ include/utils.hpp
@@ -7,6 +7,7 @@
 #include <cstring>
 
 std::string params_append (const std::vector<std::pair<std::string, std::string>>& params);
+std::string to_lower (std::string& data);
 
 std::string request(std::string url);
 static size_t write(void *contents, size_t size, size_t nmemb, void *userp);
