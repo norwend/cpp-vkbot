3042f5ba177d06b7485ceb6f9e7b451419693588
Stepan Ilyin <norwend@tuta.io>
2021-03-29 15:27:06 +0300

    Update utils.hpp

--- include/utils.hpp
+++ include/utils.hpp
@@ -8,7 +8,7 @@
 #include <curl/curl.h>
 
 std::string params_append (const std::vector<std::pair<std::string, std::string>>& params);
-std::string to_lower (std::string& data);
+std::string to_lower (std::string&& data);
 
 std::string request(std::string url);
 static size_t write(void *contents, size_t size, size_t nmemb, void *userp);
